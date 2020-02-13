d070c20595c06be1929d34d3e2679d91f6d9c74e
a-msy <m5s2y5a@gmail.com>
2020-02-14 00:34:00 +0900

    top1

--- header.php
+++ header.php
@@ -13,15 +13,13 @@
           integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link href="https://fonts.googleapis.com/css?family=Lato:400,700|Noto+Sans+JP:400,700" rel="stylesheet">
 
-    <link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>">
+    <link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>?<?php echo date('Ymd-Hi'); ?>" type="text/css">
 </head>
 <body>
-<div class="container">
-    <div class="row">
-        トップ画像的ななにか
-    </div>
+<div class="top">
+    <img src="<?php bloginfo('template_directory'); ?>/img/top.jpg">
 </div>
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+<nav class="navbar navbar-expand-lg navbar-light bg-white">
     <div class="container">
         <a class="navbar-brand" href="#">FeelNEO</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar" aria-controls="navbar"
@@ -30,12 +28,20 @@
         </button>
 
         <div class="collapse navbar-collapse" id="navbar">
-            <ul class="navbar-nav mr-auto">
-                <li class="nav-item active">
-                    <a class="nav-link" href="#">ホーム</a>
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link" href="#">HOME</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">VIDEOS</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">LYRIC</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">SCHEDULE</a>
                 </li>
             </ul>
         </div>
     </div>
-
 </nav>
--- index.php
+++ index.php
@@ -1,14 +1,50 @@
 <?php get_header(); ?>
 <main>
-    <div class="container">
-        <div class="row">
-            <div class="col-md-6">
-                aaaaa
-            </div>
-            <div class="col-md-6">
-                aaaaa
+    <section>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-3 content-box">
+                    <div class="content-title">
+                        <h1>コンテンツ一覧</h1>
+                        <p>- Contents -</p>
+                    </div>
+                </div>
+                <div class="col-md-9">
+                    <div class="row">
+                        <div class="col-md-12 content-schedule">
+                            <div class="row">
+                                <div class="col-md-6 col-md-offset-6 content-text my-parts">
+                                    <span>
+                                        <h2>スケジュール</h2>
+                                        <p>- Schedule -</p>
+                                    </span>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-12 content-lyrics">
+                            <div class="row">
+                                <div class="col-md-6 col-md-offset-6 content-text my-parts">
+                                    <span>
+                                        <h2>歌詞</h2>
+                                        <p>- Lyrics -</p>
+                                    </span>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-12 content-videos">
+                            <div class="row">
+                                <div class="col-md-6 col-md-offset-6 content-text my-parts">
+                                    <span>
+                                        <h2>動画</h2>
+                                        <p>- Videos -</p>
+                                    </span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
-    </div>
+    </section>
 </main>
 <?php get_footer(); ?>
