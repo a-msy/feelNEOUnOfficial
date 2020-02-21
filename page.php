cfaeef34cd3605ccfe358d393fb022987548a9c2
a-msy <m5s2y5a@gmail.com>
2020-02-21 20:13:12 +0900

    固定ページにパンくず追加

--- page.php
+++ page.php
@@ -4,10 +4,16 @@
  */
 get_header();
 ?>
-
 <section id="primary" class="content-area">
     <main id="main" class="site-main">
         <div class="container">
+            <div>
+                <!-- パンくずリスト -->
+                <?php
+                breadcrumb(); //パンくず
+                ?>
+                <!-- パンくずリストここまで -->
+            </div>
             <div class="row">
                 <div class="col-md-3 content-box">
                     <div class="content-box-inner">
